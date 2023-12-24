d022bcdd70a620e75b5d15d525f3c50cdd64267b
xyth0rn <49625757+xyth0rn@users.noreply.github.com>
2023-12-24 13:54:13 +0800

    Update c_lang_notes.c

--- c_lang_notes.c
+++ c_lang_notes.c
@@ -118,6 +118,25 @@ void customSplit(char *input, const char *delimiter) {
     }
 }
 
+// convert decimal to binary
+void decToBinary(int n) { 
+    // array to store binary number 
+    int binaryNum[1000]; 
+  
+    // counter for binary array 
+    int i = 0; 
+    while (n > 0) { 
+        // storing remainder in binary array 
+        binaryNum[i] = n % 2; 
+        n = n / 2; 
+        i++; 
+    } 
+  
+    // printing binary array in reverse order 
+    for (int j = i - 1; j >= 0; j--) 
+        printf("%d", binaryNum[j]); 
+}
+
 int main() {
   int a=1, b=0;
   // c has <= and >= operators
